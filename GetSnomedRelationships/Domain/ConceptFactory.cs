7dfe0c379c07dde3edccd6332478edcd67545299
Dave Lobban <dave.lobban@pushdr.com>
2019-10-10 14:43:05 +0100

    Init

---
+++ GetSnomedRelationships/Domain/ConceptFactory.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GetSnomedRelationships.Domain
+{
+    internal class ConceptFactory
+    {
+        internal static async Task<Model.Concept> GetById(int id)
+        {
+            var client = new HttpClient();
+            var url = $"https://browser.ihtsdotools.org/snowstorm/snomed-ct/v2/browser/MAIN/concepts/{id}";
+            var responseTask = client.GetStringAsync(url);
+            var response = await responseTask;
+
+            var options = new JsonSerializerOptions
+            {
+                AllowTrailingCommas = true
+            };
+
+            return JsonSerializer.Deserialize<Model.Concept>(response, options);
+        }
+    }
+}
---
+++ GetSnomedRelationships/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GetSnomedRelationships.Domain;
+using System.Threading.Tasks;
+
+namespace GetSnomedRelationships
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var ids = new List<int> {408729009, 116676008, 116683001, 736474004};
+            foreach (var id in ids)
+            {
+                var concept = await ConceptFactory.GetById(id);
+                var term = concept.Relationships.Single(concept => concept.Active).Type.Pt.Term;
+                Console.WriteLine($"{id}: {term}");
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
